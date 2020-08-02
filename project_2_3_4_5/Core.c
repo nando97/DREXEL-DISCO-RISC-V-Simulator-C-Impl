958af3a6b452a5f2a475b948fc074233e011b4c6
Fernando Mendez <fernandomendez@Dat-MacBook-587.fios-router.home>
2020-08-01 23:30:32 -0400

    very crude data memory implementation

--- project_2_3_4_5/Core.c
+++ project_2_3_4_5/Core.c
@@ -92,6 +92,18 @@ bool tickFunc(Core *core){
     Signal ALU_result, zero;
     ALU(reg1, ALU_operand1, ALU_ctrl_signal, &ALU_result, &zero);
 
+    // (Step 8) get data from data memory 
+    Signal mem_data = '\0';
+    if (control_signals.MemRead == 1)
+        mem_data = (core.data_mem[ALU_result+8] |
+                    core.data_mem[ALU_result+5] |
+                    core.data_mem[ALU_result+5] |
+                    core.data_mem[ALU_result+4] |
+                    core.data_mem[ALU_result+3] |
+                    core.data_mem[ALU_result+2] |
+                    core.data_mem[ALU_result] |
+        );
+
     // (Step N) Increment PC. FIXME, is it correct to always increment PC by 4?!
     core->PC += 4;
 
